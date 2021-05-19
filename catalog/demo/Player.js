b205c95b36a624a8425b55e51cbdc4906967fafe
Sergej067 <84158337+Sergej067@users.noreply.github.com>
2021-05-19 18:03:03 +0300

    Add files via upload

---
+++ catalog/demo/Player.js
@@ -0,0 +1,163 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    theme: '#F24751',
+    audio: [
+    			{
+            name: 'Предисловие',
+            artist: 'Anri',
+            url: 'audio/00.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 1 часть',
+            artist: 'Anri',
+            url: 'audio/01.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 2 часть',
+            artist: 'Anri',
+            url: 'audio/02.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 3 часть',
+            artist: 'Anri',
+            url: 'audio/03.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 4 часть',
+            artist: 'Anri',
+            url: 'audio/04.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 5 часть',
+            artist: 'Anri',
+            url: 'audio/05.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '1 глава 6 часть',
+            artist: 'Anri',
+            url: 'audio/06.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '2 глава 1 часть',
+            artist: 'Anri',
+            url: 'audio/07.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '2 глава 2 часть',
+            artist: 'Anri',
+            url: 'audio/08.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '2 глава 3 часть',
+            artist: 'Anri',
+            url: 'audio/09.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '2 глава 4 часть',
+            artist: 'Anri',
+            url: 'audio/10.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '2 глава 5 часть',
+            artist: 'Anri',
+            url: 'audio/11.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '3 глава 1 часть',
+            artist: 'Anri',
+            url: 'audio/12.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '3 глава 2 часть',
+            artist: 'Anri',
+            url: 'audio/13.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '3 глава 3 часть',
+            artist: 'Anri',
+            url: 'audio/14.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: '3 глава 4 часть',
+            artist: 'Anri',
+            url: 'audio/15.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 1 часть',
+            artist: 'Anri',
+            url: 'audio/16.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 2 часть',
+            artist: 'Anri',
+            url: 'audio/17.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 3 часть',
+            artist: 'Anri',
+            url: 'audio/18.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 4 часть',
+            artist: 'Anri',
+            url: 'audio/19.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 5 часть',
+            artist: 'Anri',
+            url: 'audio/20.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 6 часть',
+            artist: 'Anri',
+            url: 'audio/21.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 7 часть',
+            artist: 'Anri',
+            url: 'audio/22.mp3',
+            cover: 'cover.jpg'
+        },
+         {
+            name: ' 4 глава 8 часть',
+            artist: 'Anri',
+            url: 'audio/23.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 9 часть',
+            artist: 'Anri',
+            url: 'audio/24.mp3',
+            cover: 'cover.jpg'
+        },
+        {
+            name: ' 4 глава 10 часть',
+            artist: 'Anri',
+            url: 'audio/25.mp3',
+            cover: 'cover.jpg'
+        }
+
+    ]
+});
